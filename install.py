5e2dee947b983035abe1b465c3c5b432467eec52
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-06-19 11:48:47 +0800

    Update install.py

--- install.py
+++ install.py
@@ -25,7 +25,9 @@ if not os.path.exists(model_path):
     download(model_url, model_path)
 
 print("Checking roop requirements")
-launch.run_pip(f"install insightface-0.7.3-cp310-cp310-win_amd64.whl")
+launch.run_pip(
+    f"install whl/insightface-0.7.3-cp310-cp310-win_amd64.whl"
+)
 with open(req_file) as file:
     for package in file:
         try:
